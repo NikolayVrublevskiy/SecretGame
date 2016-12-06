3db0546ca46f967519cd4c10c52f82eb8678b280
NikolayVrublevskiy <nikolay.vrublevskiy@gmail.com>
2016-12-06 16:54:33 +0200

    Initial commit

---
+++ Intermediate/Build/Win64/UE4Editor/Development/MyProject3/MyProject3.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_MYPROJECT3_H__
+#define __AUTO_MYPROJECT3_H__
+//Last Write: 06.12.2016 13:24:29 +00:00
+#include "C:\Users\nikolay.vrublevskiy\Documents\Unreal Projects\MyProject3\Source\MyProject3\MyProject3.h"
+#endif//__AUTO_MYPROJECT3_H__
---
+++ Intermediate/Build/Win64/UE4Editor/Development/MyProject3/PCH.MyProject3.h.cpp
@@ -0,0 +1,1 @@
+#include "C:\Users\nikolay.vrublevskiy\Documents\Unreal Projects\MyProject3\Source\MyProject3\MyProject3.h"
